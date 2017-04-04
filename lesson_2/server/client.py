29331ba7b3cf66ea3ba292bebef1bc8816db6263
luckywookie <olga.belykh@gmail.com>
2017-04-04 19:39:49 +0300

    add pack and unpack of send data

--- lesson_2/server/client.py
+++ lesson_2/server/client.py
@@ -1,4 +1,9 @@
 import socket
+from datetime import datetime
+from struct import *
+
+
+timestamp = datetime.today().timestamp()
 
 HOST, PORT = 'localhost', 9999
 
@@ -7,7 +12,8 @@ print('Клиент запущен')
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
 
-sock.sendall(bytes('Ho-ho-ho!', 'utf-8'))
+p = pack('2i', 0x7A7A, int(timestamp))
+sock.sendall(p)
 recvd = str(sock.recv(1024), 'utf-8')
 
 print(recvd)
--- lesson_2/server/server.py
+++ lesson_2/server/server.py
@@ -1,11 +1,17 @@
 import socketserver
+import datetime
+from struct import *
 
 
 class TCPHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
-        self.data = self.request.recv(1024).decode()
+        self.data = self.request.recv(1024)
         print('Клиент {} пишет: {}'.format(self.client_address, self.data))
+        p = unpack('2i', self.data)
+        # d = self.data.split()[2]
+        day = datetime.datetime.fromtimestamp(float(p[0]))
+        print(day)
 
     def finish(self):
         pass

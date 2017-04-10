597de12d4aa40cc574de851b4da95fe2a952fb36
luckywookie <olga.belykh@gmail.com>
2017-04-10 15:20:18 +0300

    make all data to send it to server

--- lesson_2/server/client.py
+++ lesson_2/server/client.py
@@ -1,9 +1,10 @@
 import socket
-from datetime import datetime
-from struct import *
+from datetime import datetime, time
+from struct import pack
 
 
-timestamp = datetime.today().timestamp()
+# timestamp = datetime.today().timestamp()
+# day = datetime.datetime.fromtimestamp(float(p[0]))
 
 HOST, PORT = 'localhost', 9999
 
@@ -12,7 +13,17 @@ print('Клиент запущен')
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
 
-p = pack('2i', 0x7A7A, int(timestamp))
+now = datetime.now()
+
+_date = hex(((now.year - 2000 << 9) | (now.month << 5) | (now.day & 31)) & 0xFFFFF).encode('utf-8')
+_time = hex((now.hour << 12) | (now.minute << 6) | (now.second & 60)).encode('utf-8')
+_type = '0x00'.encode()
+_command = '0x00'.encode()
+_id_terminal = 123456
+_id_tranz = 123789
+
+p = pack('2s6s6s4s4sii', b'zz', _date, _time, _type, _command, _id_terminal, _id_tranz)
+print(p)
 sock.sendall(p)
 recvd = str(sock.recv(1024), 'utf-8')
 
--- lesson_2/server/server.py
+++ lesson_2/server/server.py
@@ -1,5 +1,4 @@
 import socketserver
-import datetime
 from struct import *
 
 
@@ -8,10 +7,19 @@ class TCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024)
         print('Клиент {} пишет: {}'.format(self.client_address, self.data))
-        p = unpack('2i', self.data)
-        # d = self.data.split()[2]
-        day = datetime.datetime.fromtimestamp(float(p[0]))
-        print(day)
+        p = unpack('2s6s6s4s4sii', self.data)
+        _date = int(p[1], 16)
+        year = (_date & 0xfe00) >> 9
+        month = (_date & 0x1f0) >> 5
+        day = _date & 0x1f
+        print('Дата: 20{}.{}.{}'.format(year, month, day))
+        _time= int(p[2], 16)
+        hour = (_time & 0x1f000) >> 12
+        mins = (_time & 0xfc0) >> 6
+        sec = _time & 0x3f
+        print('Время: {}:{}:{}'.format(hour, mins, sec))
+        print('Тип транзакции: {}, команда: {}'.format(p[3], p[4]))
+        print('Терминал: {}, операция: {}'.format(p[5], p[6]))
 
     def finish(self):
         pass

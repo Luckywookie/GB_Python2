a7a0a12b593a540508855eec5cfb1de2f2228761
luckywookie <olga.belykh@gmail.com>
2017-04-15 20:33:41 +0300

    add select functions

--- lesson_2/server/models/payments.py
+++ lesson_2/server/models/payments.py
@@ -5,14 +5,14 @@ from db import Base
 class PaymentModel(Base):
     __tablename__ = 'payment'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    datetime = Column(DateTime)
+    datetm = Column(DateTime)
     terminal_id = Column(Integer, ForeignKey('terminal.id'))
     transaction_id = Column(Integer)
     partner_id = Column(Integer, ForeignKey('partner.id'))
     summ = Column(Integer)
 
-    def __init__(self, datetime, terminal_id, transaction_id, partner_id=None, summ=None):
-        self.datetime = datetime
+    def __init__(self, datetm, terminal_id, transaction_id, partner_id=None, summ=None):
+        self.datetm = datetm
         self.terminal_id = terminal_id
         self.transaction_id = transaction_id
         self.partner_id = partner_id
@@ -21,7 +21,7 @@ class PaymentModel(Base):
     def json(self):
         return {
             'id': self.id,
-            'datetime': self.datetime,
+            'datetime': self.datetm,
             'terminal_id': self.terminal_id
         }
 
--- lesson_2/server/run_db.py
+++ lesson_2/server/run_db.py
@@ -1,15 +1,15 @@
-from datetime import datetime
+from datetime import datetime, date
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from models.partners import PartnerModel
 from models.payments import PaymentModel
 from models.terminals import TerminalModel
 from models.balans import CreditModel, DebitModel
+from select_data import search_date, search_partner_trans
 
 from db import Base
 
 # metadata = MetaData()
-# Session = sessionmaker()
 
 engine = create_engine('sqlite:///data.db', echo=True)
 
@@ -23,19 +23,20 @@ Base.metadata.create_all(engine)
 Sess = sessionmaker(bind=engine)
 session = Sess()
 
+# поиск транвакций в диапазоне дат
+print(search_date(session, PaymentModel, date(2011, 3, 5), date(2019, 3, 5)))
 
-p3 = PartnerModel(title='Firmaaaa')
-PartnerModel.save_to_db(p3, session)
-# print(p1.find_by_id(session.query(PartnerModel), 1))
+# поиск в диапазоне дат партнеров, которым должны денег
+print(search_partner_trans(session, PaymentModel, date(2011, 3, 5), date(2019, 3, 5)))
 
-# query = session.query(PaymentModel).first()
-# print(query.datetime)
-# now = datetime.now()
-#
-# t1 = TerminalModel(configuration=123, title='Black terminal')
-# p1 = PartnerModel(title='Firma OOO')
-# tr1 = PaymentModel(datetime=now, terminal_id=1, transaction_id=1, partner_id=1, summ=123456)
-#
+# join
+print(session.query(PaymentModel, PartnerModel).join(PartnerModel, PaymentModel.partner_id == PartnerModel.id).all())
+
+
+t1 = TerminalModel(configuration=1258, title='Grey terminal')
+p1 = PartnerModel(title='Lakomka OOO')
+tr1 = PaymentModel(datetm=date(2015, 12, 15), terminal_id=7, transaction_id=15, partner_id=7, summ=152856)
+
 # session.add(t1)
 # session.add(p1)
 # session.add(tr1)

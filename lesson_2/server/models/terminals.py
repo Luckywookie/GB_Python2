23f8d03a61f4be8d8e5b07e04d09805e6cb5bc18
luckywookie <olga.belykh@gmail.com>
2017-04-20 23:08:08 +0300

    add update and delete methods

--- lesson_2/server/models/terminals.py
+++ lesson_2/server/models/terminals.py
@@ -38,3 +38,24 @@ class TerminalModel(Base):
         session.add(self)
         session.commit()
 
+    @classmethod
+    def delete_partner(cls, _id):
+        find_terminal = session.query(TerminalModel).filter(TerminalModel.id == _id)
+        exist_terminal = find_terminal.first()
+        if exist_terminal:
+            find_terminal.delete()
+            session.commit()
+            print("Terminal deleted")
+        else:
+            print("Bad request")
+
+    @classmethod
+    def update_partner(cls, _id, new_title):
+        find_terminal = session.query(TerminalModel).filter(TerminalModel.id == _id)
+        exist_terminal = find_terminal.first()
+        if exist_terminal:
+            find_terminal.update({'title': new_title})
+            session.commit()
+            print("<Terminal: {}>".format(new_title))
+        else:
+            print("Bad request")
